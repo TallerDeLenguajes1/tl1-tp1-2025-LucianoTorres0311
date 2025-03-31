fba20fdb406d7e24db91a7f2d175b5525b3b11fe
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-03-31 16:16:33 -0300

    archvio con actualizacion de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("Hola mundo");
+    int x = 6;
+    int *p = &x;
+    printf("Hola mundo \n");
+    printf("el contenido del puntero es: %d \n",*p);
+    printf("La direccion de memoria guardada por el puntero es: %p \n",p);
+    printf("La direccion de memoria de la variable es: %p \n", &x);
+    printf("La direccion de memoria del puntero es: %p \n",&p);
+    printf("tamaño de memoria ocupado por la variable: %d \n",   sizeof(x));
     return 0;
 }
