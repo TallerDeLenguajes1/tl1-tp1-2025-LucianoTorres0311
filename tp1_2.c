d2dc041896720ed7c45a58a7a306df40e38a622b
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-04-01 19:24:34 -0300

    funcion mostrarDireccion agregada

--- tp1_2.c
+++ tp1_2.c
@@ -2,16 +2,19 @@
 
 int devolverCuadrado(int num1);
 void escribirCuadrado(int num2);
+void mostrarDireccion(int *punt);
 
 int main()
 {
     int num1, num2;
     int result;
+    int *punt = &num1;
     printf("Ingrese dos numeros: \n");
     scanf("%d %d", &num1 , &num2);
     result = devolverCuadrado(num1);
     printf("El cuadrado del numero calculado es: %d \n", result);
     escribirCuadrado(num2);
+    mostrarDireccion(punt);
     return 0;
 }
 
@@ -27,5 +30,12 @@ void escribirCuadrado(int num2)
 {
     int resultado;
     resultado = num2 * num2;
-    printf("El cuadrado calculado es igual a: %d", resultado);
+    printf("El cuadrado calculado es igual a: %d\n", resultado);
+}
+
+void mostrarDireccion(int *punt)
+{
+    printf("Direccion de memoria de la variable num1: %p \n", punt);
+    printf("Contenido de la variable num1: %d \n", *punt);
+
 }
