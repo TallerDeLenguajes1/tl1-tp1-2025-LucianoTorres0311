3da23935d18b338f200a8baedaba5335a1e488f0
Luciano Torres <lucianotorreslanzavecchia@gmail.com>
2025-04-01 19:53:12 -0300

    funcion invertir y cambios para codigo mas legible

--- tp1_2.c
+++ tp1_2.c
@@ -3,18 +3,30 @@
 int devolverCuadrado(int num1);
 void escribirCuadrado(int num2);
 void mostrarDireccion(int *punt);
+void invertir(int *punt, int *punt2);
 
 int main()
 {
     int num1, num2;
     int result;
     int *punt = &num1;
+    int *punt2 = &num2;
     printf("Ingrese dos numeros: \n");
     scanf("%d %d", &num1 , &num2);
+    printf("El valor antes de invertir de num1 es: %d \n",num1);
+    printf("El valor antes de invertir de num2 es: %d \n",num2);
+    printf("\n");
     result = devolverCuadrado(num1);
     printf("El cuadrado del numero calculado es: %d \n", result);
+    printf("\n");
     escribirCuadrado(num2);
+    printf("\n");
     mostrarDireccion(punt);
+    printf("\n");
+    invertir(punt,punt2);
+    printf("El valor despues de invertir de num1 es: %d \n",num1);
+    printf("El valor despues de invertir de num2 es: %d \n",num2);
+    printf("\n");
     return 0;
 }
 
@@ -38,4 +50,14 @@ void mostrarDireccion(int *punt)
     printf("Direccion de memoria de la variable num1: %p \n", punt);
     printf("Contenido de la variable num1: %d \n", *punt);
 
+}
+
+void invertir(int *punt, int *punt2)
+{
+    int aux;
+    aux = *punt;
+    *punt = *punt2;
+    *punt2 = aux;
+    
+    
 }
